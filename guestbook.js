0b2ab312484519f4ae35648ed58057008b145b6a
Dion Hobdy <devdionhobdy@gmail.com>
2022-07-04 03:07:39 -0700

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://f9a15cf4.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
